2274e390c1f10cc7660002a44795d987486fb47e
Gabriel Toban <gtoban87@gmail.com>
2019-08-20 15:04:33 -0500

    add keras test

--- kerasNN.py
+++ kerasNN.py
@@ -6,22 +6,36 @@ print(tf.version.VERSION)
 print(tf.keras.__version__)
 
 model = tf.keras.Sequential([
-layers.Dense(64, activation="relu", input_shape=(3000,)),
-layers.Dense(64, activation="relu"),
-layers.Dense(10, activation="softmax")])
+layers.Dense(3000, activation="relu", input_shape=(3000,)),
+layers.Dense(3000, activation="relu"),
+layers.Dense(3000, activation="relu"),
+layers.Dense(3000, activation="relu"),
+layers.Dense(3000, activation="relu"),
+layers.Dense(2, activation="softmax")])
 
 model.compile(optimizer=tf.train.AdamOptimizer(0.001),
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+                  loss=tf.keras.losses.categorical_crossentropy,
+                  metrics=[tf.keras.metrics.categorical_accuracy])
 
+record_count = 0
+with open("input002.csv") as f:
+    for line in f:
+        if (line.strip()):
+            record_count += 1
 f = open("input002.csv")
 line = f.readline().strip()
-data = np.zeros(3000,)
-labels = np.zeros(1,)
+data = np.zeros((record_count,3000))
+labels = np.zeros((record_count,2))
 sample = 0
 while (line):
     arow = line.split(",")
-    labels[sample] = 1 if arow[0] ==
+    labels[sample][1 if arow[0] == 'W' else 0] = 1
+    measure_count = 0
+    for ame in arow[1:]:
+        data[sample][measure_count] = ame
     
+    sample += 1
     line = f.readline().strip()
-                                                  
+    
+model.fit(data, labels, epochs=30, batch_size=record_count)
+
