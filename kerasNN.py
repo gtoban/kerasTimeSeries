237dbfbd0175d0dd84971db0a7a222997e056573
Gabriel Toban <gtoban87@gmail.com>
2019-08-29 17:01:22 -0500

    1-D Convolutionpython3 kerasNN.py

--- kerasNN.py
+++ kerasNN.py
@@ -1,20 +1,19 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv1D, Flatten
 
 print(tf.version.VERSION)
 print(tf.keras.__version__)
 
-model = tf.keras.Sequential([
-layers.Dense(3000, activation="relu", input_shape=(3000,)),
-layers.Dense(3000, activation="relu"),
-layers.Dense(3000, activation="relu"),
-layers.Dense(3000, activation="relu"),
-layers.Dense(3000, activation="relu"),
-layers.Dense(2, activation="softmax")])
+model = Sequential()
+model.add(Conv1D(filters=32, kernel_size=10, activation="relu", input_shape=(3000,1))) #shape batch, steps, channels
+model.add(Conv1D(filters=16, kernel_size=10, activation="relu"))
+model.add(Flatten())
+model.add(Dense(2, activation="softmax"))
 
 model.compile(optimizer=tf.train.AdamOptimizer(0.001),
-                  loss=tf.keras.losses.categorical_crossentropy,
+                  loss=tf.keras.losses.binary_crossentropy,
                   metrics=[tf.keras.metrics.categorical_accuracy])
 
 record_count = 0
@@ -36,6 +35,9 @@ while (line):
     
     sample += 1
     line = f.readline().strip()
-    
+
+print("shape:", data.shape)
+data = np.expand_dims(data,axis=2)
+print("shape:", data.shape)
 model.fit(data, labels, epochs=30, batch_size=record_count)
 
