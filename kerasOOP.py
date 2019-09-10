0d7519486eb4a56db80772c99e5b4a3ab1761c01
Gabriel Toban <gtoban87@gmail.com>
2019-09-10 16:36:32 -0500

    almost done for convolution model selection

--- kerasOOP.py
+++ kerasOOP.py
@@ -1,8 +1,12 @@
 import numpy as np
+import json
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv1D, Flatten
-
+from tensorflow.keras.layers import Dense, Conv1D, Flatten, MaxPooling1D
+from tensorflow.keras import backend as K
+from tensorflow.keras import metrics
+from sklearn.model_selection import KFold
+from sklearn.metrics import f1_score, confusion_matrix, multilabel_confusion_matrix
 
 class ann_data(object):
     def __init__(self):
@@ -22,7 +26,7 @@ class ann_data(object):
         sample = 0
         while (line):
             arow = line.split(",")
-            labels[sample][1 if arow[0] == 'W' else 0] = 1
+            labels[sample][1 if arow[0] == 'R' else 0] = 1
             measure_count = 0
             for ame in arow[1:]:
                 data[sample][measure_count] = ame
@@ -33,20 +37,168 @@ class ann_data(object):
         data = np.expand_dims(data,axis=2)
         #print("shape:", data.shape)
         return data, labels, record_count
-
+    
 class keras_ann(object):
     def __init__(self):
         self.dataPath = ""
         self.outputPath = ""
+        self.inputShape = (3000,1)
+        self.metric_Obj = ann_data()
 
-    def convModel(self, no_filters=[32,16]):
+    def f1_score(self, y_true, y_pred):
+        #y_true = get_value(y_true_tf)
+        #y_pred = get_value(y_pred_tf)
+        #true_neg = 0
+        #true_pos = 0
+        #false_neg = 0
+        #flase_pos = 0
+        #for i in range(y_true.shape[0]):
+        #    true = np.argmax(y_true[i])
+        #    pred = np.argmax(y_pred[i])
+        #    if (pred == 1):
+        #        if (true == pred):
+        #            true_pos += 1
+        #        else:
+        #            false_pos += 1
+        #    else:
+        #        if (true == pred):
+        #            true_neg += 1
+        #        else:
+        #            false_neg += 1
+        #precision = true_pos/(true_pos+false_pos)
+        #recall = true_pos/(true_pos+false_neg)        
+        #f1_score =  2 * ((precision*recall)/(precision+recall))
+        ##f1_score, update_op = tf.contrib.metrics.f1_score(y_true_tf,y_pred_tf)
+        #return to_dense(f1_score)
+
+        #=============
+        # SOURCE:
+        # https://stackoverflow.com/questions/43547402/how-to-calculate-f1-macro-in-keras
+        #=============
+        
+        true_pos = K.sum(K.round(K.clip(y_true*y_pred,0,1)))
+        poss_pos = K.sum(K.round(K.clip(y_true,0,1)))
+        recall = true_pos / (poss_pos + K.epsilon())
+        pred_pos = K.sum(K.round(K.clip(y_true,0,1)))
+        precision = true_pos/(pred_pos + K.epsilon())
+        f1_score =  2 * ((precision*recall)/(precision+recall+K.epsilon()))
+        
+    def convModel(self, modelArgs=[
+        {
+            'layer': 'conv1d',
+            'no_filters': 64,
+            'kernal_size':10,
+            'activation':'relu'
+        },{
+            'layer': 'conv1d',
+            'no_filters': 32,
+            'kernal_size':10,
+            'activation':'relu'
+        },{
+            'layer': 'flatten'
+        },{
+            'layer': 'dense',
+            'output': 2,
+            'activation':'softmax'
+        },{
+            'layer': 'compile',
+            'optimizer': 'adam',
+            'loss': 'categorical_crossentropy',
+            'metrics':['acc']
+        }]):
         model = Sequential()
-        model.add(Conv1D(filters=no_filters[0], kernel_size=10, activation="relu", input_shape=(3000,1))) #shape batch, steps, channels
-        model.add(Conv1D(filters=no_filters[1], kernel_size=10, activation="relu"))
-        model.add(Flatten())
-        model.add(Dense(2, activation="softmax"))
-        model.compile(optimizer='adam', #tf.train.AdamOptimizer(0.001),
-                    loss='categorical_crossentropy', #tf.keras.losses.categorical_crossentropy,
-                    metrics=['acc']) #tf.keras.metrics.categorical_accuracy
+
+        #
+        # For First Layer, input requried
+        #
+        modelArg= modelArgs[0]
+        if (modelArg['layer'] == 'conv1d'):
+            model.add(Conv1D(filters=modelArg['no_filters'], kernel_size=modelArg['kernal_size'], activation=modelArg['activation'], input_shape=self.inputShape)) #shape batch, steps, channels
+        elif (modelArg['layer'] == 'dense'):
+                model.add(Dense(activation=modelArg['activation'], input_shape=self.inputShape))
+            
+        for modelArg in modelArgs[1:]:
+            if (modelArg['layer'] == 'conv1d'):
+                model.add(Conv1D(filters=modelArg['no_filters'], kernel_size=modelArg['kernal_size'], activation=modelArg['activation'])) #shape batch, steps, channels
+            elif (modelArg['layer'] == 'flatten'):
+                model.add(Flatten())
+
+            elif (modelArg['layer'] == 'dense'):
+                model.add(Dense(modelArg['output'], activation=modelArg['activation']))
+            elif (modelArg['layer'] == 'maxpool1d'):
+                model.add(MaxPooling1D(pool_size=modelArg['pool_size'],
+                                           strides=modelArg['strides'],
+                                           padding=modelArg['padding']))
+            elif (modelArg['layer'] == 'avgpool1d'):
+                model.add(AveragePooling1D(pool_size=modelArg['pool_size'],
+                                           strides=modelArg['strides'],
+                                           padding=modelArg['padding']))
+            elif (modelArg['layer'] == 'compile'):
+                for i in range(len(modelArg['metrics'])):
+                    if (modelArg['metrics'][i] == 'f1score'):
+                        modelArg['metrics'][i] = self.f1_score
+                        break
+                model.compile(optimizer=modelArg['optimizer'], #tf.train.AdamOptimizer(0.001),
+                    loss=modelArg['loss'], #tf.keras.losses.categorical_crossentropy,
+                    metrics=[metrics.CategoricalAccuracy(), metrics.TrueNegatives(), metrics.TruePositives()]) #tf.keras.metrics.categorical_accuracy
+        #model.add(Conv1D(filters=no_filters[0], kernel_size=10, activation="relu", input_shape=self.inputShape)) #shape batch, steps, channels
+        #model.add(Conv1D(filters=no_filters[1], kernel_size=10, activation="relu"))
+        #model.add(Flatten())
+        #model.add(Dense(2, activation="softmax"))
+        #model.compile(optimizer='adam', #tf.train.AdamOptimizer(0.001),
+        #            loss='categorical_crossentropy', #tf.keras.losses.categorical_crossentropy,
+        #            metrics=['acc']) #tf.keras.metrics.categorical_accuracy
         return model
 
+    def parameterSearch(self, paramSets, X, Y, numSplits):
+        # create CV dat LOOV 
+        #numSplits = 2
+        Kf = KFold(n_splits=numSplits)
+        #for each parameter set
+        # make a model
+        #
+        #X = [0,1,2,3,4,5,6,7,8,9]
+        modelFile = open("fileModel.csv", 'w')
+        resultFile = open("fileResult.csv",'w')
+        resultFile.write("modelNum|True Neg|False Neg|True Pos|False Pos|F1_score|Acc\n")
+        modelNum = 0
+        for paramSet in paramSets:
+            #print(paramSet)
+            
+            modelFile.write(str(modelNum) + "|")
+            json.dump(paramSet, modelFile)
+            modelFile.write("\n")
+            
+            
+            try:
+                model = self.convModel(paramSet)            
+                j = 0
+                for trainInd, testInd in Kf.split(X):
+                    model.fit(X[trainInd], Y[trainInd], batch_size=None, verbose=0)
+                    Ypred = np.zeros((testInd.shape[0],Y.shape[1]))
+                    Yi = 0
+                    for pred in np.argmax(model.predict(X[testInd]), axis=1):
+                        Ypred[Yi][pred] = 1
+                        Yi += 1
+
+                    #print(Y[testInd], Ypred)
+                    confusionMatrix = multilabel_confusion_matrix(Y[testInd], Ypred)
+                    #confusionMatrix = confusion_matrix(np.argmax(Y[testInd], axis=1), np.argmax(Ypred, axis=1))
+                    #print(confusionMatrix)
+                    #print('f1_score:',f1_score(Y[testInd], Ypred, average='macro'))
+                    resultFile.write(str(modelNum) + "|")
+                    for row in confusionMatrix:
+                        for el in row:
+                            resultFile.write(str(el) + "|")
+
+                    resultFile.write(str(f1_score(Y[testInd], Ypred, average='macro')) + "|\n")
+                
+                    j+=1
+            
+            except (Exception):
+                resultFile.write("error\n")
+            modelNum+=1
+        modelFile.close()
+        resultFile.close()
+
+        
--- tune.py
+++ tune.py
@@ -11,21 +11,374 @@ def main():
     myData = ann_data()
 
     data,labels,recordCount = myData.readData()
-    print(data.shape)
-    print(labels.shape)
+    #print(data.shape)
+    #print(labels.shape)
     data = data[:10] #int(data.shape[0]/4)]
     labels = labels[:10] #int(labels.shape[0]/4)]
-    
-    model = KerasClassifier(build_fn=myAnn.convModel, epochs=2,batch_size=recordCount)
 
+    #model = myAnn.convModel()
+    #exit()
+    
+    #model = KerasClassifier(build_fn=myAnn.convModel, epochs=2,batch_size=recordCount)
+   
     #model.fit(data, labels, epochs=1, batch_size=recordCount)
-    params = dict(no_filters=[[128,64],[64,32],[32,16]])
+    #params = dict(no_filters=[[128,64],[64,32],[32,16]])
+    #params = {'modelArgs': getModels()}
+    #params = getModels()
+    modelArgs = getModels()
+    addToModels(modelArgs)
+    myAnn.parameterSearch(modelArgs,data,labels,2)
+    #random_search = RandomizedSearchCV(model, params, cv=3) #, scoring='acc')
+    #random_search_results = random_search.fit(data,labels)
+    #
+    #print(f"Best: {random_search_results.best_score_} using {random_search_results.best_params_}")
+    #print(random_search_results.best_score_)
+    #print(random_search_results.best_params_)
 
-    random_search = RandomizedSearchCV(model, params, cv=3)
-    random_search_results = random_search.fit(data,labels)
+def addToModels(modelArgs):
+    #low freq to high freq
+    modelArgs.append(
+        [
+        {
+            'layer': 'conv1d',
+            'no_filters': 66,
+            'kernal_size':5,
+            'activation':'relu'
+        },{
+            'layer': 'conv1d',
+            'no_filters': 20,
+            'kernal_size':10,
+            'activation':'relu'
+        },{
+            'layer': 'conv1d',
+            'no_filters': 10,
+            'kernal_size':15,
+            'activation':'relu'
+        },{
+            'layer': 'conv1d',
+            'no_filters': 5,
+            'kernal_size':20,
+            'activation':'relu'
+        },{
+            'layer': 'conv1d',
+            'no_filters': 3,
+            'kernal_size':25,
+            'activation':'relu'
+        },{
+            'layer': 'flatten'
+        },{
+            'layer': 'dense',
+            'output': 2,
+            'activation':'softmax'
+        },{
+            'layer': 'compile',
+            'optimizer': 'adam',
+            'loss': 'categorical_crossentropy',
+            'metrics':['acc']
+        }])
+    #high to low freq
+    modelArgs.append(
+        [
+        {
+            'layer': 'conv1d',
+            'no_filters': 3,
+            'kernal_size':25,
+            'activation':'relu'
+        },{
+            'layer': 'conv1d',
+            'no_filters': 5,
+            'kernal_size':20,
+            'activation':'relu'
+        },{
+            'layer': 'conv1d',
+            'no_filters': 10,
+            'kernal_size':15,
+            'activation':'relu'
+        },{
+            'layer': 'conv1d',
+            'no_filters': 20,
+            'kernal_size':10,
+            'activation':'relu'
+        },{
+            'layer': 'conv1d',
+            'no_filters': 66,
+            'kernal_size':5,
+            'activation':'relu'
+        },{
+            'layer': 'flatten'
+        },{
+            'layer': 'dense',
+            'output': 2,
+            'activation':'softmax'
+        },{
+            'layer': 'compile',
+            'optimizer': 'adam',
+            'loss': 'categorical_crossentropy',
+            'metrics':['acc']
+        }])
+
+    layers5 = [{
+            'layer': 'conv1d',
+            'no_filters': 3,
+            'kernal_size':25,
+            'activation':'relu'
+        },{
+            'layer': 'conv1d',
+            'no_filters': 5,
+            'kernal_size':20,
+            'activation':'relu'
+        },{
+            'layer': 'conv1d',
+            'no_filters': 10,
+            'kernal_size':15,
+            'activation':'relu'
+        },{
+            'layer': 'conv1d',
+            'no_filters': 20,
+            'kernal_size':10,
+            'activation':'relu'
+        },{
+            'layer': 'conv1d',
+            'no_filters': 66,
+            'kernal_size':5,
+            'activation':'relu'
+        }]
+    # A small model for each of the 5 conv layers with and without maxpooling. Potential preparation for ensemble.
+    for layer in layers5:
+        #without max pooling
+        modelArgs.append([])
+        modelArgs[-1].append(layer)
+        modelArgs[-1].append({
+            'layer': 'flatten'
+        })
+        modelArgs[-1].append({
+            'layer': 'dense',
+            'output': 2,
+            'activation':'softmax'
+        })
+        modelArgs[-1].append({
+            'layer': 'compile',
+            'optimizer': 'adam',
+            'loss': 'categorical_crossentropy',
+            'metrics':['acc']
+        })
+
+        #with max pooling
+        modelArgs.append([])
+        modelArgs[-1].append(layer)
+        modelArgs[-1].append({
+            'layer': 'maxpool1d',
+            'pool_size': 10,
+            'strides':None,
+            'padding':'valid'
+        })
+        modelArgs[-1].append({
+            'layer': 'flatten'
+        })
+        modelArgs[-1].append({
+            'layer': 'dense',
+            'output': 2,
+            'activation':'softmax'
+        })
+        modelArgs[-1].append({
+            'layer': 'compile',
+            'optimizer': 'adam',
+            'loss': 'categorical_crossentropy',
+            'metrics':['acc']
+        })
     
-    print(f"Best: {random_search_results.best_score_} using {random_search_results.best_params_}")
-    print(random_search_results.best_score_)
-    print(random_search_results.best_params_)
+    #Mix and match features randomly (5 convolution layers (order: 5->120, 4->120, 3->60, 2->20, total->320), maxPooling (5) (1600), number of dense layers (1-5) etc (nodes: 50,100,200,400,500,800))
+    possibleNumOfNodes = [50,100,200,400,500,800]
+    possiblePoolSize = [5,10,20,40,80]
+    for i in range(100):
+        modelArgs.append([])
+        #how many conv layers
+        numConLayers = np.random.randint(2,6)
+
+        #add a random list of layers of specified length
+        for layerIndex in np.random.randint(0,5,size=numConLayers):
+            modelArgs[-1].append(layers5[layerIndex])
+            # add maxpooling?
+            if (np.random.randint(2) == 1):
+                modelArgs[-1].append({
+                    'layer': 'maxpool1d',
+                    'pool_size': possiblePoolSize[np.random.randint(len(possiblePoolSize))],
+                    'strides':None,
+                    'padding':'valid'})
+        # add a flatten layer
+        modelArgs[-1].append({
+            'layer': 'flatten'
+        })
+        
+        # pick a random number of dense layers primarily picking 1
+        numDenseLayers = min(max(int(np.random.normal(1,scale=2.5)), 1),5)
+        for layerNum in range(numDenseLayers-1):
+            modelArgs[-1].append({
+                'layer': 'dense',
+                'output': possibleNumOfNodes[np.random.randint(len(possibleNumOfNodes))],
+                'activation':'softmax'})
+        modelArgs[-1].append({
+            'layer': 'dense',
+            'output': 2,
+            'activation':'softmax'
+        })
+        modelArgs[-1].append({
+            'layer': 'compile',
+            'optimizer': 'adam',
+            'loss': 'categorical_crossentropy',
+            'metrics':['acc']
+        })
+    
+def getModels():
+    modelArgs = []
+    #modelArgs.append(
+    #    [
+    #    {
+    #        'layer': 'conv1d',
+    #        'no_filters': 64,
+    #        'kernal_size':10,
+    #        'activation':'relu'
+    #    },{
+    #        'layer': 'conv1d',
+    #        'no_filters': 32,
+    #        'kernal_size':10,
+    #        'activation':'relu'
+    #    },{
+    #        'layer': 'flatten'
+    #    },{
+    #        'layer': 'dense',
+    #        'output': 2,
+    #        'activation':'softmax'
+    #    },{
+    #        'layer': 'compile',
+    #        'optimizer': 'adam',
+    #        'loss': 'categorical_crossentropy',
+    #        'metrics':['acc']
+    #    }])
+    #
+    #modelArgs.append(
+    #    [
+    #    {
+    #        'layer': 'conv1d',
+    #        'no_filters': 128,
+    #        'kernal_size':10,
+    #        'activation':'relu'
+    #    },{
+    #        'layer': 'conv1d',
+    #        'no_filters': 64,
+    #        'kernal_size':10,
+    #        'activation':'relu'
+    #    },{
+    #        'layer': 'flatten'
+    #    },{
+    #        'layer': 'dense',
+    #        'output': 2,
+    #        'activation':'softmax'
+    #    },{
+    #        'layer': 'compile',
+    #        'optimizer': 'adam',
+    #        'loss': 'categorical_crossentropy',
+    #        'metrics':['acc']
+    #    }])
+
+    #Automatic Sleep Stage Scoring with Single-Channel EEG Using CNNs
+    modelArgs.append(
+        [
+        {
+            'layer': 'conv1d',
+            'no_filters': 20,
+            'kernal_size':200,
+            'activation':'relu'
+        },{
+            'layer': 'maxpool1d',
+            'pool_size': 20,
+            'strides':None,
+            'padding':'valid'
+        },{
+            'layer': 'conv1d',
+            'no_filters': 400,
+            'kernal_size':20,
+            'activation':'relu'
+        },{
+            'layer': 'maxpool1d',
+            'pool_size': 10,
+            'strides':None,
+            'padding':'valid'
+        },{
+            'layer': 'flatten'
+        },{
+            'layer': 'dense',
+            'output': 500,
+            'activation':'relu'
+        },{
+            'layer': 'dense',
+            'output': 500,
+            'activation':'relu'
+        },{
+            'layer': 'dense',
+            'output': 2,
+            'activation':'softmax'
+        },{
+            'layer': 'compile',
+            'optimizer': 'adam',
+            'loss': 'categorical_crossentropy',
+            'metrics':['acc']
+        }])
+
+    #Real-time human activity recognition from accelerometer data using CNN
+    modelArgs.append(
+        [
+        {
+            'layer': 'conv1d',
+            'no_filters': 196,
+            'kernal_size':12,
+            'activation':'relu'
+        },{
+            'layer': 'maxpool1d',
+            'pool_size': 4,
+            'strides':None,
+            'padding':'valid'
+        },{
+            'layer': 'flatten'
+        },{
+            'layer': 'dense',
+            'output': 500,
+            'activation':'relu'
+        },{
+            'layer': 'dense',
+            'output': 2,
+            'activation':'softmax'
+        },{
+            'layer': 'compile',
+            'optimizer': 'adam',
+            'loss': 'categorical_crossentropy',
+            'metrics':['acc']
+        }])
+
+
+    return modelArgs
+
+def f1_score(self, y_true, y_pred):
+    true_neg = 0
+    true_pos = 0
+    false_neg = 0
+    flase_pos = 0
+    for i in len(y_true):
+        true = np.argmax(y_true[i])
+        pred = np.argmax(y_pred[i])
+        if (pred == 1):
+            if (true == pred):
+                true_pos += 1
+            else:
+                false_pos += 1
+        else:
+            if (true == pred):
+                true_neg += 1
+            else:
+                false_neg += 1
+    precision = true_pos/(true_pos+false_pos)
+    recall = true_pos/(true_pos+false_neg)
+    return 2 * ((precision*recall)/(precision+recall))
+
 
 main()
